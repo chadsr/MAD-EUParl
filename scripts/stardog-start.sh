f83bf265c58b981c5d05339582a2a3b8bd8c4220
Ross <ross@rchadwick.co.uk>
2017-02-23 14:34:27 +0100

    Create table on start

--- scripts/stardog-start.sh
+++ scripts/stardog-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd $STARDOG_HOME/bin
-./stardog-admin server start --disable-security
+./stardog-admin server start --disable-security && ./stardog-admin db create -o reasoning.type=DL reasoning.sameas=FULL -n parlialytics 
